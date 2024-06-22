13caadd427241e22933124fb9f897f679610ab68
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-06-22 21:03:15 +0300

    implements get single candidate

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UpdateCandidate from "./UpdateCandidate";
-
+import { getCandidate } from "../services";
+import { useParams } from "react-router-dom";
 
 const Candidate = () => {
-    return(
-        <>
-            <UpdateCandidate />
-        </>
-    )
+    const { candidateId } = useParams();
+    const [candidate, setCandidate] = useState(null);
+
+    useEffect(() => {
+        const fetchCandidate = async () => {
+            const response = await getCandidate(candidateId);
+            setCandidate(response.candidate);
+        }
+        fetchCandidate();
+    }, [candidateId]);
+
+    return (
+        <div>
+            {candidate ? (
+                <>
+                    <h2>Candidate Details</h2>
+                    <ul>
+                        <li><strong>FirstName: </strong>{candidate.firstName}</li>
+                        <li><strong>LastName: </strong>{candidate.lastName}</li>
+                        <li><strong>Email: </strong>{candidate.email}</li>
+                        <li><strong>PhoneNumber: </strong>{candidate.phoneNumber}</li>
+                        <li><strong>TimeInterval: </strong>{candidate.timeInterval}</li>
+                        <li><strong>LinkedIn: </strong>{candidate.linkedIn}</li>
+                        <li><strong>Github: </strong>{candidate.github}</li>
+                        <li><strong>Comment: </strong>{candidate.comment}</li>
+                    </ul>
+                    <UpdateCandidate />
+                </>
+            ) : (
+                <h4>Candidate Not Found!</h4>
+            )}
+        </div>
+    );
 }
 
 export default Candidate;
